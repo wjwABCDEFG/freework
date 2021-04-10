78042b6aef37d3c88c38fbcab0eb2ea8378565fb
wjwABCDEFG <975504808@qq.com>
2021-04-10 20:37:25 +0800

    模拟登录

---
+++ freework-job/freework-job-service/src/main/java/com/wjw/job/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.wjw.job.controller;
+
+import com.wjw.common.entity.Result;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wjw
+ * @date 2021/4/9 18:48
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/job/user")
+public class UserController {
+
+    @PostMapping("/login")
+    public Result login(String username, String password){
+        return Result.ok().data("token", "123123");
+    }
+
+    @GetMapping("/info")
+    public Result getInfo(){
+        return Result.ok().data("roles", "[admin]").data("name", "admin");
+    }
+}
