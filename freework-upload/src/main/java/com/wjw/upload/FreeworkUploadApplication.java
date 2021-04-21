289ae0039b896d170912e174b15638c0fb48ccc6
wjwABCDEFG <975504808@qq.com>
2021-04-21 22:52:29 +0800

    修复上传模块报错需要数据源问题

--- freework-upload/src/main/java/com/wjw/upload/FreeworkUploadApplication.java
+++ freework-upload/src/main/java/com/wjw/upload/FreeworkUploadApplication.java
@@ -2,12 +2,13 @@ package com.wjw.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author wjw
  * @date 2021/3/4 17:52
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FreeworkUploadApplication {
 
     public static void main(String[] args) {
