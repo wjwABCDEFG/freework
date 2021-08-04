7d4d1569e83827ceb662fc6d9081aff29094719e
wjwABCDEFG <975504808@qq.com>
2021-08-05 00:46:44 +0800

    声网api，用于面试视频通话

--- src/views/system/serviceListener.vue
+++ src/views/system/serviceListener.vue
@@ -1,27 +1,51 @@
 <template>
   <div>
-    <!-- 123 -->
-    <!-- <iframe
-      src="http://localhost:8848/nacos/#/serviceManagement"
-      frameborder="1"
-      width="500"
-      height="600"
-      scrolling="auto"
-    ></iframe> -->
+    <el-card class="box-card">
+      <el-table :data="serviceList" style="width: 100%">
+        <el-table-column prop="name" label="服务名" width="180"></el-table-column>
+        <el-table-column prop="clusterCount" label="集群数目"></el-table-column>
+        <el-table-column prop="ipCount" label="实例数目"></el-table-column>
+        <el-table-column prop="healthyInstanceCount" label="健康数目"></el-table-column>
+      </el-table>
+    </el-card>
+    <el-link type="primary" class="link-css" href="http://localhost:8848/nacos/#/serviceManagement?dataId=&group=&appName=&namespace=" target="_blank" rel="noopener noreferrer">点击查看详情</el-link>
   </div>
 </template>
 
 <script>
-// let data = "";
-// const ifr = document.createElement("iframe");
-// ifr.src = "http://localhost:8848/nacos/#/serviceManagement";
-// ifr.style.display = "none";
-// document.body.appendChild(ifr);
-// ifr.onload = function () {
-//   ifr.onload = function () {
-//     data = ifr.contentWindow.name;
-//     console.log("收到数据:", data);
-//   };
-//   ifr.src = "http://localhost:8848/nacos/#/serviceManagement";
-// };
+export default {
+  data() {
+    return {
+      serviceList: [],
+    };
+  },
+
+  methods: {
+    getService() {
+      this.$http
+        .get(
+          `http://localhost:8848/nacos/v1/ns/catalog/services?hasIpCount=true&withInstances=false&pageNo=1&pageSize=10&serviceNameParam=&groupNameParam=&accessToken=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYWNvcyIsImV4cCI6MTYyMDY1MDQ5M30.kzKJUUsuDfAH4j5GTfylotAMeI7gX-qPg7n9UBVWyFQ&namespaceId=`
+        )
+        .then((resp) => {
+          // console.log(resp.data);
+          this.serviceList = resp.data.serviceList;
+        });
+    },
+  },
+
+  created() {
+    this.getService();
+  },
+};
 </script>
+
+<style>
+.box-card{
+  width: 70%;
+  margin: 20px auto;
+}
+
+.link-css{
+  margin-left: 15%;
+}
+</style>
